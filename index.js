741949b76f6ae92244bbe4cecf9e7be97c4032d2
IFTC-XLKJ <iftcceo@139.com>
2024-12-07 13:19:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ window.xingicp = null
 onload = e => {
     console.log(e)
     xingicp = new pgdbs(dbs_5c724bd3de34a72c601f1c9449c9a193fd039dbd8025a670ca098a88a2ebdeb8)
+    icp.onkeydown = async e => {
+        if (e.key == 'Enter') {
+            const json = await xingicp.getTableData({
+                filter: `ICP='${icp.value}'`,
+                limit: 1,
+                page: 1,
+            })
+            const res = json.fields
+            if (res.length == 0) {
+            } else {
+            }
+        }
+    }
 }

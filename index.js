cd29d362923f732f5beda1fbea7b9cee75bbda00
IFTC-XLKJ <iftcceo@139.com>
2024-12-07 13:54:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ onload = e => {
                         </div>
                         <div class="card-body">
                             <h6 class="card-subtitle mb-2 text-muted">备案者：${email}</h6>
-                            <p class="card-text"></p>
+                            <p class="card-text">${sites.map(site => { return `<a href="https://${site}">${site}</a>` }).join("<br/>")}</p>
                         </div>
                     </div>`
                 }

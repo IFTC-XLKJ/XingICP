37c6afd47e51ad7d149fc06f212e97e30c88073f
IFTC-XLKJ <iftcceo@139.com>
2024-12-07 13:39:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 window.xingicp = null
+
+function isValidDomain(domain) {
+    const domainRegex = /^(?!:\/\/)(?=.{1,255}$)((?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,})$/;
+    return domainRegex.test(domain);
+}
+
 onload = e => {
     console.log(e)
     xingicp = new pgdbs(dbs_5c724bd3de34a72c601f1c9449c9a193fd039dbd8025a670ca098a88a2ebdeb8)
     icp.onkeydown = async e => {
         if (e.key == 'Enter') {
-            const json = await xingicp.getTableData({
-                filter: `ICP='${icp.value}'`,
-                limit: 1,
-                page: 1,
-            })
-            const res = json.fields
-            if (res.length == 0) {
+            if (icp.value.trim().replaceAll(" ", "") && Number(icp.value.trim().replaceAll(" ", "")) != "number" && icp.value.trim().replaceAll(" ", "").length == 8) {
+                const json = await xingicp.getTableData({
+                    filter: `ICP=${icp.value}`,
+                    limit: 1,
+                    page: 1,
+                })
+                const res = json.fields
+                if (res.length == 0) {
+                } else {
+                    const ICP = res[0].ICP.replaceAll("a", "")
+                    const email = res[0].email
+                    const sites = res[0].site.split(",")
+                    console.log(ICP, email, sites)
+                }
             } else {
+                alert('请输入正确的ICP')
             }
         }
     }

710978a5a68ac7f7df54cb80dfec33897abc5b7e
IFTC-XLKJ <iftcceo@139.com>
2024-12-07 14:41:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,22 @@ onload = e => {
                         </div>
                         <div class="card-body">
                             <h6 class="card-subtitle mb-2 text-muted">备案者：${email}</h6>
-                            <p class="card-text">备案域名和作品：(${sites.length}个)<br>${sites.map(site => { return `<a href="https://${site}">${site}</a>` }).join("<br/>")}</p>
+                            <p class="card-text">备案域名和作品：(${sites.length}个)<br>${sites
+                            .map(site => {
+                                const dd = site.split("->")
+                                if (dd[0] == "coco") {
+                                    return `<a href="https://coco.codemao.cn/editor/player/${dd[1]}?channel=h5">Coco${d[1]}</a>`
+                                } else if (dd[0] == "kn") {
+                                    return `<a href="https://kn.codemao.cn/player?type=2&workId=${dd[1]}">KN${d[1]}</a>`
+                                } else if (dd[0] == "kitten") {
+                                    return `<a href="https://player.codemao.cn/new/${dd[1]}">Kitten${dd[1]}</a>`
+                                } else if (dd[0] == "nemo") {
+                                    return `<a href="https://nemo.codemao.cn/w/${dd[1]}/${dd[1]}">Nemo${d[1]}</a>`
+                                } else if (dd[0] == "voto") {
+                                    return `<a href="https://voto.pages.dev/player/${dd[1]}">Voto${dd[1]}</a>`
+                                }
+                                return `<a href="https://${site}">${site}</a>`
+                            }).join("<br/>")}</p>
                         </div>
                     </div>`
                 }
